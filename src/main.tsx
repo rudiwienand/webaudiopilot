5111abf70ccc97c5644fc7bd6bd2e2cad1b74e82
rudiwienand <rudiwienand@gmail.com>
2026-01-29 14:51:24 +0100

    Fix missing entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App';
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
